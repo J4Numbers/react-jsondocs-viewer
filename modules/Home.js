b6d17973ef3585f68cb05bf7dadbd14b0b29e24f
Jayne Doe <j4numbers@gmail.com>
2018-12-29 17:13:19 +0000

    chore(linting): brought in an eslint standard (ongoing)

--- modules/Home.js
+++ modules/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 /**
  * The Home component describes the view for the homepage of the documentation server.
@@ -9,5 +9,5 @@ import React from 'react'
 export default (props) => {
     return (
         <h2>Home</h2>
-    )
-}
+    );
+};
